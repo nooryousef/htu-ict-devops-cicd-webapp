6b33375c0536e81d54f0675927c2f9330ebbccdf
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-18 21:53:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,11 @@
 
 #Install all required project dependencies.
 
-sudo apt install python3
+sudo apt install python3 -m venv
 sudo apt  install python3-pip
 sudo apt install gunicorn
+sudo apt install python3 -m venv
+
 
 
 
