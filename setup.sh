6fbad60c53ab1b844ad03d421c6a0c18cbc17af2
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-18 22:53:53 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,18 +12,20 @@ sudo apt install python3
 sudo apt  install python3-pip
 sudo apt install gunicorn
 sudo apt-get install python3-venv
+
 python3 -m venv venv
+sudo apt-get install -y python3-venv
+
 
 source venv/bin/activate
 pip install -r requirements.txt
 
-sudo apt-get install -y python3-venv
 
 
 
 gunicorn --bind=0.0.0.0 --workers=4 hello:app 
 export FLASK_APP=hello.py
-flask un
+
 
 
 
