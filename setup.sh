5d9732e25a7f9928eb76b95e6f0ac8f0285a99c2
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-18 23:08:35 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,12 +15,11 @@ sudo apt-get install python3-venv
 
 python3 -m venv venv
 sudo apt-get install -y python3-venv
-
+sudo apt pip install Flask
 
 source venv/bin/activate
 pip install -r requirements.txt
 
-pip install Flask
 
 
 gunicorn --bind=0.0.0.0 --workers=4 hello:app 
