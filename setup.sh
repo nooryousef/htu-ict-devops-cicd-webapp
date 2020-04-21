e2d8c738dbe87edc67f8c6539ac2102b4ec65b75
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-22 00:04:18 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
 
+
+sudo apt install python3
+
+sudo apt-get install python-virtualenv
+
+python3 -m venv venv
+sudo apt install gunicorn
+
+pip install -r requirements.txt
+source venv/bin/activate
